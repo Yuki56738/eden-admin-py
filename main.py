a2f6868360631211451bf374e2c1c9b895083766
Yuki <risaton@risaton.net>
2023-01-17 17:53:46 +0900

    commit

--- main.py
+++ main.py
@@ -914,32 +914,6 @@ async def ticket(ctx: ApplicationContext):
     await txt1.send(f"問題が作成されました。ただいま対応しますので、少々お待ちください... {ctx.user.mention}")
 
 
-# @bot.slash_command(description="メニューを表示")
-# async def menu(ctx: ApplicationContext):
-#     global vcRole
-#     global vcTxt
-#     global txtMsg
-#     global guildsettings
-#     # flag = False
-#     try:
-#         txt1 = vcTxt[str(ctx.author.voice.channel.id)]
-#         txt1 = bot.get_channel(txt1)
-#         flag = True
-#     except:
-#         # print(traceback.format_exc())
-#         # traceback.print_exc()
-#         print(traceback.format_exc())
-#         return
-#
-#
-#     res = await ctx.respond(view=MyViewChangeRoomName(), delete_after=3 * 60)
-#
-#     res2 = await ctx.send(view=MyViewChangeRoomLimit(), delete_after=3 * 60)
-#     # await res.delete_original_message()
-# await res2.delete_original_message()
-# await ctx.send(view=MyViewRoomNolook())
-# await txt1.send(view=MyViewChangeRoomName())
-# await txt1.send(view=MyViewChangeRoomLimit())
 
 
 @bot.slash_command(description="メニューを表示")
@@ -1182,45 +1156,6 @@ async def ping(ctx: ApplicationContext):
     await ctx.respond(embed=Embed(description=f"レイテンシーは、{lat * 60}ms."))
 
 
-# @bot.slash_command(guild_ids=[977138017095520256])
-# async def hello(ctx: ApplicationContext):
-#     await ctx.respond("Hello!")
-
-"""
-@bot.slash_command(description="設定を表示する")
-async def set_see(ctx: ApplicationContext):
-    global vcRole
-    global vcTxt
-    global txtMsg
-    global guildsettings
-    if not ctx.author.guild_permissions.administrator:
-        return
-    # with open("tmp.json", "w", encoding="utf8")as f:
-    #     json.dump(guildsettings[str(ctx.guild.id)], f, ensure_ascii=False)
-    # with open("")
-    # data = json.loads(str(guildsettings[str(ctx.guild.id)]).replace("'", '"'))
-    await ctx.respond(
-        embed=Embed(description=json.dumps(guildsettings[str(ctx.guild.id)], indent=2, ensure_ascii=False)))
-
-
-@bot.slash_command(description="設定を保存して適応する")
-async def set_save(ctx: ApplicationContext, json1: Option(str, name="json", required=True, description="設定のjson")):
-    global vcRole
-    global vcTxt
-    global txtMsg
-    global guildsettings
-    if not ctx.author.guild_permissions.administrator:
-        return
-    try:
-        guildsettings[str(ctx.guild.id)] = json.loads(json1)
-        print(json1)
-        save_guild_settings()
-    except Exception as e:
-        await ctx.respond(embed=Embed(description=f"エラー。設定は保存されていません！: {e}"))
-        return
-    await ctx.respond(embed=Embed(description="設定を保存しました。"))
-"""
-
 
 def save_to_json():
     global vcRole
@@ -1244,18 +1179,6 @@ def save_to_json():
     print("Saved bot state.")
 
 
-# @bot.slash_command(description="Translate to Japanese.")
-# async def japanese(ctx: ApplicationContext, *, text):
-#     translator = deepl.Translator(DEEPL_KEY)
-#     result = translator.translate_text(text, target_lang='JA')
-#     await ctx.respond(text)
-#     await ctx.send(result)
-# @bot.slash_command(description="Translate to Japanese.")
-# async def english(ctx: ApplicationContext, *, text):
-#     translator = deepl.Translator(DEEPL_KEY)
-#     result = translator.translate_text(text, target_lang='EN')
-#     await ctx.respond(text)
-#     await ctx.send(result)
 
 def save_guild_settings():
     global vcRole
