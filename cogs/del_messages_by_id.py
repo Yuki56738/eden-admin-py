0f085f08e5eb7d7f66edf1316b234cef259c88d6
Yuki <risaton@risaton.net>
2023-03-30 12:39:39 +0900

    commit

--- cogs/del_messages_by_id.py
+++ cogs/del_messages_by_id.py
@@ -19,7 +19,7 @@ class DelMsgById(Cog):
         self.bot: Bot
         ctx.followup: Webhook
         # await ctx.user.send(view=self.ModalToConfirmDelete)
-        await ctx.send_modal(self.ModalToConfirmDelete(title="Are you sure to do this?"))
+        await ctx.send_modal(self.ModalToConfirmDelete(title="Are you sure to do this?", user_id=user_id, channel_id=channel_id))
 
 
         # toDeleteember = await self.bot.fetch_user(int(user_id))
@@ -75,34 +75,41 @@ class DelMsgById(Cog):
     async def on_ready(self):
         print("DelMsgById ready.")
     class ModalToConfirmDelete(discord.ui.Modal):
-        def __init__(self, *args, **kwargs) -> None:
+        def __init__(self, user_id:str ,channel_id: str, *args, **kwargs) -> None:
             super().__init__(*args, **kwargs)
             self.add_item(discord.ui.InputText(label="Type yes in uppercase..."))
         async def callback(self, interaction: Interaction):
             interaction.response: InteractionResponse
-            await interaction.response.send_message("Executing...")
-            toDeleteember = await self.bot.fetch_user(int(user_id))
-            print(toDeleteember.name, toDeleteember.id)
-            channs = await interaction.guild.fetch_channels()
-            for chann in channs:
-                # if not chann is TextChannel:
-                #     continue
-                # if chann == TextChannel:
-                #     print(3)
-                try:
-                    msgs = await chann.history(limit=10000).flatten()
-                except:
-                    continue
-                for x in msgs:
-                    if x.author == toDeleteember:
-                        if channel_id is None:
-                            print('deleting:', x.content)
-                            await x.delete()
-                        elif not channel_id is None and int(x.channel.id) == int(channel_id):
-                            print('deleting at channel:', self.bot.get_channel(int(channel_id)).name)
-                            print('deleting:', x.content)
-                            await x.delete()
-            await ctx.followup.send('削除しました。')
+            # flag = False
+            if self.children[0].value == "YES":
+                await interaction.response.send_message("Executing...")
+                # flag = True
+            else:
+                await interaction.response.send_message('Type "yes" in uppercase!')
+                return
+
+            # toDeleteember = await self.bot.fetch_user(int(user_id))
+            # print(toDeleteember.name, toDeleteember.id)
+            # channs = await interaction.guild.fetch_channels()
+            # for chann in channs:
+            #     # if not chann is TextChannel:
+            #     #     continue
+            #     # if chann == TextChannel:
+            #     #     print(3)
+            #     try:
+            #         msgs = await chann.history(limit=10000).flatten()
+            #     except:
+            #         continue
+            #     for x in msgs:
+            #         if x.author == toDeleteember:
+            #             if channel_id is None:
+            #                 print('deleting:', x.content)
+            #                 await x.delete()
+            #             elif not channel_id is None and int(x.channel.id) == int(channel_id):
+            #                 print('deleting at channel:', self.bot.get_channel(int(channel_id)).name)
+            #                 print('deleting:', x.content)
+            #                 await x.delete()
+            # await ctx.followup.send('削除しました。')
 
 
 def setup(bot):
